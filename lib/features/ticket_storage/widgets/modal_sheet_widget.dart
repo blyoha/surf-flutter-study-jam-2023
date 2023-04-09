4fb8e3381dbb15b7cdcae7f56240a3a5c01d150e
blyoha <filling_box@yahoo.com>
2023-04-09 16:54:50 +0300

    Task 2

--- lib/features/ticket_storage/widgets/modal_sheet_widget.dart
+++ lib/features/ticket_storage/widgets/modal_sheet_widget.dart
@@ -9,6 +9,8 @@ class ModalSheet extends StatefulWidget {
 }
 
 class _ModalSheetState extends State<ModalSheet> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final textController = TextEditingController();
@@ -20,39 +22,61 @@ class _ModalSheetState extends State<ModalSheet> {
       ),
       child: Column(
         children: [
-          TextFormField(
-            controller: textController,
-            decoration: InputDecoration(
-              label: const Text("Введите URL"),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                    // color: Colors.amberAccent,
-                    ),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                  color: Colors.redAccent,
+          Form(
+            key: formKey,
+            child: TextFormField(
+              controller: textController,
+              decoration: InputDecoration(
+                label: const Text("Введите URL"),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                      // color: Colors.amberAccent,
+                      ),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: Colors.redAccent,
+                  ),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: Colors.redAccent,
+                  ),
                 ),
               ),
+              validator: (value) {
+                if (value!.isNotEmpty &&
+                    RegExp(r"\bhttps?\S*pdf\b").hasMatch(value)) {
+                  return null;
+                } else {
+                  return "Введите корректный URL";
+                }
+              },
             ),
-            validator: validateUrl,
           ),
           const Gap(12.0),
           MaterialButton(
-            onPressed: () {},
+            onPressed: () async {
+              final isValid = formKey.currentState!.validate();
+
+              if (isValid) {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Билет добавлен в список!"),
+                  duration: Duration(seconds: 2),
+                ));
+                Navigator.pop(context);
+              }
+            },
             child: const Text("Добавить"),
           ),
         ],
       ),
     );
-  }
-
-  String? validateUrl(String? value) {
-    return null;
   }
 }
